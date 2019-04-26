39dee6b14ff54201b2baf0e7be042562eab83b1a
sathak <sathak318@gmail.com>
2019-04-26 15:12:51 +0530

    test commit

--- src/components/viewItem.js
+++ src/components/viewItem.js
@@ -6,7 +6,7 @@ import { actionViewItemCreators } from '../actions/listings-action';
 import './viewItem.css'
 class viewItem extends Component {
 
-
+//view Methods
     componentWillMount() {
         // This method runs when the component is first added to the page
         const id = this.props.match.params.id;
@@ -20,13 +20,8 @@ class viewItem extends Component {
         return (
             <div className="row viewItem">
 
-<<<<<<< HEAD
-                {this.props.Item.map((itm,i) =>
+                {this.props.Item.map((itm, i) =>
                     <div className="col-md-offset-1" key={i}>
-=======
-                {this.props.Item.map(itm =>
-                    <div className="col-md-offset-1">
->>>>>>> 02df4d64282ca021d33a7c6f76b6234189faa369
                         <div className="Item-header">
                             <Link to="/">
                                 <span>Home</span>
@@ -44,13 +39,8 @@ class viewItem extends Component {
 
                                 <div className="preview-pic tab-content">
                                     <div className="tab-pane active" id="pic-1">
-<<<<<<< HEAD
-                                        {this.props.Item.map((itm,i)  =>
+                                        {this.props.Item.map((itm, i) =>
                                             <img src={require(`../image/${itm.id}.png`)} key={i} alt="image" />
-=======
-                                        {this.props.Item.map(itm =>
-                                            <img src={require(`../image/${itm.id}.png`)} />
->>>>>>> 02df4d64282ca021d33a7c6f76b6234189faa369
                                         )}
                                     </div>
                                 </div>
@@ -72,24 +62,14 @@ class viewItem extends Component {
                                     <p className="card-text">RM 1200</p>
                                 </div>
                                 <p className="Item-pricelabel">Item Condition</p>
-<<<<<<< HEAD
-                                {this.props.Item.map((itm,i)  =>
+                                {this.props.Item.map((itm, i) =>
                                     <div className="Item-info" key={i}>
-=======
-                                {this.props.Item.map(itm =>
-                                    <div className="Item-info">
->>>>>>> 02df4d64282ca021d33a7c6f76b6234189faa369
                                         <p className="card-text">{itm.attributes.condition}</p>
                                     </div>
                                 )}
                                 <p className="Item-pricelabel">Item Location</p>
-<<<<<<< HEAD
-                                {this.props.Item.map((itm,i)  =>
+                                {this.props.Item.map((itm, i) =>
                                     <div className="Item-info" key={i}>
-=======
-                                {this.props.Item.map(itm =>
-                                    <div className="Item-info">
->>>>>>> 02df4d64282ca021d33a7c6f76b6234189faa369
                                         <p className="card-text">{itm.attributes.location}</p>
                                     </div>
                                 )}
@@ -98,13 +78,8 @@ class viewItem extends Component {
                                     <div className="col-md-2 col-xs-2">
                                         <i className="fa fa-user-circle sellerIcon" aria-hidden="true"></i>
                                     </div>
-<<<<<<< HEAD
-                                    {this.props.Item.map((itm,i)  =>
+                                    {this.props.Item.map((itm, i) =>
                                         <div className="col-md-10 col-xs-10" key={i}>
-=======
-                                    {this.props.Item.map(itm =>
-                                        <div className="col-md-10 col-xs-10">
->>>>>>> 02df4d64282ca021d33a7c6f76b6234189faa369
                                             <p className="seller-info-text1">{itm.attributes.seller_name}</p>
                                             <p className="seller-info-text2">{itm.attributes.seller_type}</p>
                                         </div>
@@ -112,25 +87,20 @@ class viewItem extends Component {
                                 </div>
                                 <div className="col-md-12 boderTop webInfo">
                                     <div className="row">
-                                    Interested withe the ad? Contact the seller
+                                        Interested withe the ad? Contact the seller
                                     </div>
-<<<<<<< HEAD
-                                    {this.props.Item.map((itm,i)  =>
-                                    <div className="row" key={i}>
-=======
-                                    {this.props.Item.map(itm =>
-                                    <div className="row">
->>>>>>> 02df4d64282ca021d33a7c6f76b6234189faa369
-                                    <a className="btn btn-outline-danger btnWidth"><i className="fa fa-phone"></i><span className="padLeft">{itm.attributes.phone}</span></a>
-                                    </div>
+                                    {this.props.Item.map((itm, i) =>
+                                        <div className="row" key={i}>
+                                            <a className="btn btn-outline-danger btnWidth"><i className="fa fa-phone"></i><span className="padLeft">{itm.attributes.phone}</span></a>
+                                        </div>
                                     )}
                                     <div className="row">
-                                    <a className="btn btn-outline-danger btnWidth"><i className="fa fa-envelope"></i><span className="padLeft">Email</span></a>
+                                        <a className="btn btn-outline-danger btnWidth"><i className="fa fa-envelope"></i><span className="padLeft">Email</span></a>
                                     </div>
                                     <div className="row">
-                                    <a className="btn btn-danger btnWidth"><i className="fa fa-wechat"></i><span className="padLeft">Chat</span></a>
+                                        <a className="btn btn-danger btnWidth"><i className="fa fa-wechat"></i><span className="padLeft">Chat</span></a>
                                     </div>
-</div>
+                                </div>
                             </div>
                             <div className="col-md-1"></div>
                         </div>
@@ -148,13 +118,8 @@ class viewItem extends Component {
                                     <div className="tab-content tabs">
                                         <h3></h3>
                                         <div role="tabpanel" className="tab-pane fade in active" id="Section1">
-<<<<<<< HEAD
-                                            {this.props.Item.map((itm,i)  =>
+                                            {this.props.Item.map((itm, i) =>
                                                 <p className="borderLine" key={i}>{itm.attributes.description}</p>
-=======
-                                            {this.props.Item.map(itm =>
-                                                <p className="borderLine">{itm.attributes.description}</p>
->>>>>>> 02df4d64282ca021d33a7c6f76b6234189faa369
                                             )}
                                         </div>
 
@@ -250,24 +215,19 @@ class viewItem extends Component {
                     </div>
                 </div>
                 <div className="col-md-12 boderTop mobInfo">
-                                   
-<<<<<<< HEAD
-                                    {this.props.Item.map((itm,i)  =>
-                                    <div className="col-xs-4" key={i}>
-=======
-                                    {this.props.Item.map(itm =>
-                                    <div className="col-xs-4">
->>>>>>> 02df4d64282ca021d33a7c6f76b6234189faa369
-                                    <a className="btn btn-outline-danger btn-sm btnWidth"><i className="fa fa-phone"></i><span className="padLeft">{itm.attributes.phone}</span></a>
-                                    </div>
-                                    )}
-                                    <div className="col-xs-4">
-                                    <a className="btn btn-outline-danger btn-sm btnWidth"><i className="fa fa-envelope"></i><span className="padLeft">Email</span></a>
-                                    </div>
-                                    <div className="col-xs-4">
-                                    <a className="btn btn-danger btn-sm btnWidth"><i className="fa fa-wechat"></i><span className="padLeft">Chat</span></a>
-                                    </div>
-</div>
+
+                    {this.props.Item.map((itm, i) =>
+                        <div className="col-xs-4" key={i}>
+                            <a className="btn btn-outline-danger btn-sm btnWidth"><i className="fa fa-phone"></i><span className="padLeft">{itm.attributes.phone}</span></a>
+                        </div>
+                    )}
+                    <div className="col-xs-4">
+                        <a className="btn btn-outline-danger btn-sm btnWidth"><i className="fa fa-envelope"></i><span className="padLeft">Email</span></a>
+                    </div>
+                    <div className="col-xs-4">
+                        <a className="btn btn-danger btn-sm btnWidth"><i className="fa fa-wechat"></i><span className="padLeft">Chat</span></a>
+                    </div>
+                </div>
             </div>
         )
     }
