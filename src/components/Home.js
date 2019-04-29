b2252ab3072d31e9924c8878c1fcef92472f077b
smartdevelopers <sathakathullas@ilink-systems.com>
2019-04-29 12:45:46 +0300

    loading

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,44 +3,46 @@ import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { actionProductListCreators } from '../actions/listings-action';
-
+import LoadingSpinner from './LoadingSpinner'
 
 
 
 class Home extends Component {
-//Home Method
-   
+    //Home Method
+    
     componentWillMount() {
-       
-        this.props.requestProductLists();
+        this.setState({ loading: true }, () => {
+            this.props.requestProductLists();
+        });
     }
 
     render() {
-        
+
         return (
 
             <div>
                 <h3>Listings</h3>
-                <div className="col-md-12">
-                {this.props.products.map((home,index) =>
-                        <div className="col-md-2 col-sm-6 col-xs-6" key={index}>
-                            <Link to={'/viewItem/'+home.id}>
-                                <div className="card" >
-                                    <img className="card-img-top"  src={require(`../image/${home.id}.png`)} alt="Card image cap" />
-                                    <div className="card-body">
-                                        <p className="card-title">{home.attributes.title}</p>
-                                        
-                                    </div>
-                                  <div className="card-footer">
-                                  <p className="card-text">{home.attributes.price}</p>
-                                  </div>
-                                </div>
-                            </Link>
-                        </div>
-                    )}
-                    </div>
+                {this.props.loading ? <LoadingSpinner /> :
+                    <div className="col-md-12">
+                        {this.props.products.map((home, index) =>
+                            <div className="col-md-2 col-sm-6 col-xs-6" key={index}>
+                                <Link to={'/viewItem/' + home.id}>
+                                    <div className="card" >
+                                        <img className="card-img-top" src={require(`../image/${home.id}.png`)} alt="" />
+                                        <div className="card-body">
+                                            <p className="card-title">{home.attributes.title}</p>
 
-                
+                                        </div>
+                                        <div className="card-footer">
+                                            <p className="card-text">{home.attributes.price}</p>
+                                        </div>
+                                    </div>
+                                </Link>
+                            </div>
+                        )}
+                    </div>
+                }
+
             </div>
 
         );
