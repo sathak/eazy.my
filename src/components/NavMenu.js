d7167fae374a18ec5a3cdece596c0fda53aab996
smartdevelopers <sathakathullas@ilink-systems.com>
2019-04-29 13:01:20 +0300

    removed glyphicon

--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -1,6 +1,6 @@
 ﻿import React from 'react';
 import { Link } from 'react-router-dom';
-import { Glyphicon, Nav, Navbar, NavItem } from 'react-bootstrap';
+import { Nav, Navbar, NavItem } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 import './NavMenu.css';
 
